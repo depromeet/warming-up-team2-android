381332e3b2046030eb821030f5495d3d77175cbe
zion830 <zion830@honglab.org>
2019-10-08 00:43:15 +0900

    Koin 초기화 클래스 추가

---
+++ app/src/main/java/com/depromeet/android/childcare/di/MainApplication.kt
@@ -0,0 +1,17 @@
+package com.depromeet.android.childcare.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            modules(apiModule)
+        }
+    }
+}
