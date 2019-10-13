2def0028743537ff1cb2f4f1ff51caf7039a3c71
zion830 <zion830@honglab.org>
2019-10-14 04:11:14 +0900

    BaseFragment 추가

---
+++ app/src/main/java/com/depromeet/android/childcare/base/BaseFragment.kt
@@ -0,0 +1,25 @@
+package com.depromeet.android.childcare.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int
+) : Fragment(layoutResId) {
+    protected lateinit var binding: B
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    fun showToast(msg: String) = Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+}
