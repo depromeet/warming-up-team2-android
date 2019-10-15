0168bcff1b9a18f3e33c06937f20bbadbf8e331e
kyung <leeseok8347@gmail.com>
2019-10-15 18:13:10 +0900

    edit viewmodel to use local variable

--- app/src/main/java/com/depromeet/android/childcare/home/HomeViewModel.kt
+++ app/src/main/java/com/depromeet/android/childcare/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.depromeet.android.childcare.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.depromeet.android.childcare.data.FeedDataSource
@@ -12,11 +13,14 @@ class HomeViewModel(
     private val toastProvider: ToastProvider
 ) : ViewModel() {
 
-    val feeds = MutableLiveData<List<Record>>()
+    private val _feeds = MutableLiveData<List<Record>>()
+
+    val feeds : LiveData<List<Record>>
+        get() = _feeds // _feeds를 feed를 통해 발행합니다
 
     init {
         feedRepository.getAllFeeds({
-            feeds.value = it
+            _feeds.value = it
         }, {
             toastProvider.makeToast("값 가져오는 중에 에러남")
         })
