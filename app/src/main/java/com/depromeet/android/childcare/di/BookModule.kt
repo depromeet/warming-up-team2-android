378adbc8c316d43a3cf08c86a89d0180eaf67e1c
zion830 <zion830@honglab.org>
2019-10-18 22:31:45 +0900

    가계부 di 설정

---
+++ app/src/main/java/com/depromeet/android/childcare/di/BookModule.kt
@@ -0,0 +1,14 @@
+package com.depromeet.android.childcare.di
+
+import com.depromeet.android.childcare.book.BookViewModel
+import com.depromeet.android.childcare.data.BookDataSource
+import com.depromeet.android.childcare.data.BookRepository
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val bookModule = module {
+
+    single<BookDataSource> { BookRepository(get()) }
+
+    viewModel { BookViewModel(get()) }
+}
