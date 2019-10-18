b53fbb0267059ae6a917b746e88d79e9e3873109
zion830 <zion830@honglab.org>
2019-10-18 22:27:00 +0900

    요약 정보에 필요한 문자열 추가

--- app/src/main/java/com/depromeet/android/childcare/AppConstant.kt
+++ app/src/main/java/com/depromeet/android/childcare/AppConstant.kt
@@ -1,6 +1,6 @@
 package com.depromeet.android.childcare
 
-const val BASE_URL = "https://"
+const val BASE_URL = "https://test.com"
 const val TIME_OUT_SEC = 5L
 
 const val EXTRA_BOOK_POSITION = "EXTRA_BOOK_POSITION"
