1b18744344db4210ce4d810d134ea53a8b646a16
zion830 <zion830@honglab.org>
2019-10-26 14:38:28 +0900

    di module 정리

--- app/src/main/java/com/depromeet/android/childcare/di/AppModule.kt
+++ app/src/main/java/com/depromeet/android/childcare/di/AppModule.kt
@@ -2,6 +2,8 @@ package com.depromeet.android.childcare.di
 
 import com.depromeet.android.childcare.data.FeedDataSource
 import com.depromeet.android.childcare.data.FeedRepository
+import com.depromeet.android.childcare.util.ResourcesProvider
+import com.depromeet.android.childcare.util.ResourcesProviderImpl
 import com.depromeet.android.childcare.util.ToastProvider
 import com.depromeet.android.childcare.util.ToastProviderImpl
 import org.koin.dsl.module
@@ -11,4 +13,6 @@ val appModule = module {
     single<ToastProvider> { ToastProviderImpl(get()) }
 
     single<FeedDataSource> { FeedRepository() }
+
+    single<ResourcesProvider> { ResourcesProviderImpl(get()) }
 }
