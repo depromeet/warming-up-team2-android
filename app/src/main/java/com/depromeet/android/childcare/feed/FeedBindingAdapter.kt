7b4f85ede53d6f4934ab3df381a709623ac88e53
kyung <leeseok8347@gmail.com>
2019-11-07 22:43:57 +0900

    add feed more btn

--- app/src/main/java/com/depromeet/android/childcare/feed/FeedBindingAdapter.kt
+++ app/src/main/java/com/depromeet/android/childcare/feed/FeedBindingAdapter.kt
@@ -1,8 +1,11 @@
 package com.depromeet.android.childcare.feed
 
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.depromeet.android.childcare.R
+import com.depromeet.android.childcare.model.PaymentType
 import com.depromeet.android.childcare.model.Record
 
 @BindingAdapter("bind_feed_type")
@@ -25,4 +28,12 @@ fun RecyclerView.setBookDetailsItems(feedType: FeedType) {
 @BindingAdapter("bind_feed_items")
 fun RecyclerView.setFeedItems(items : List<Record>) {
     (adapter as FeedRecyclerViewAdapter).setItems(items)
+}
+
+@BindingAdapter("bind_payment_type")
+fun AppCompatTextView.setPaymentType(paymentType: PaymentType) {
+    text = when(paymentType) {
+        PaymentType.CARD -> resources.getString(R.string.feed_payment_card)
+        PaymentType.CASH -> resources.getString(R.string.feed_payment_cash)
+    }
 }
