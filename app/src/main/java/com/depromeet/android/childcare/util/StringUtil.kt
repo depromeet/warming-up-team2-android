75065d5ecb85d46e485568a16a0621fab2cc34ad
zion830 <zion830@honglab.org>
2019-11-08 21:34:35 +0900

    디폴트로 오늘 날짜 뜨도록 구현

--- app/src/main/java/com/depromeet/android/childcare/util/StringUtil.kt
+++ app/src/main/java/com/depromeet/android/childcare/util/StringUtil.kt
@@ -4,15 +4,22 @@ import android.graphics.Typeface
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 // 숫자에 3자리 단위로 comma 삽입
 fun insertComma(num: Int): String = DecimalFormat("#,###").format(num)
+
 fun insertComma(num: Float): String = DecimalFormat("#,###").format(num)
 
 fun getBoldText(text: String, name: String): SpannableStringBuilder {
     val str = SpannableStringBuilder(text)
     val textPosition = text.indexOf(name)
-    str.setSpan(android.text.style.StyleSpan(Typeface.BOLD),
-        textPosition, textPosition + name.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    str.setSpan(
+        android.text.style.StyleSpan(Typeface.BOLD),
+        textPosition, textPosition + name.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
     return str
 }
+
+fun getDateString() = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(Date())
